8a820d59c6864c4d61948836db60e32bdc780bb3
Jeffrey <1737723363@qq.com>
2017-10-25 22:50:24 +0800

    Add : Login Form & Change Auth Routes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,41 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@extends('layouts.admin-frame')
+@section('body')
+	<div class="container">
+		<div class="login-box">
+			<div>
+				<div class="login-form row">
+					<div class="col-sm-12 text-center login-header">
+						<i class="login-logo fa fa-connectdevelop fa-5x"></i>
+						<h4 class="login-title">Flat Admin V2</h4>
+					</div>
+					<div class="col-sm-12">
+						<div class="login-body">
+							<div class="progress hidden" id="login-progress">
+								<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+									Log In...
+								</div>
+							</div>
+							<form action="{{ route('login') }}" method="post" id="login-form">
+								<div class="control">
+									<input type="text" name="email" class="form-control" placeholder="请输入账户" />
+								</div>
+								<div class="control">
+									<input type="password" name="password" class="form-control" placeholder="请输入密码" />
+								</div>
+								<div class="control">
+									<input type="checkbox" name="remember"> 记住登录
+								</div>
+								<div class="login-button text-center">
+									<input type="submit" class="btn btn-primary" value="Login">
+								</div>
+							</form>
+						</div>
+						<div class="login-footer">
+							<span class="text-right"><a href="javascript:void(0)" class="color-white">Forgot password?</a></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
