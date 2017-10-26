6246478d6482441959ea9ab92c6b38340c6b37c3
Jeffrey <1737723363@qq.com>
2017-10-26 08:34:18 +0800

    Finish:Reset admin password script

--- database/seeds/ResetAdminSeeder.php
+++ database/seeds/ResetAdminSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class ResetAdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		echo "开始重置Admin密码\n";
+		$user = User::where('name', 'admin')->first();
+		if (!$user) {
+			$user = new User();
+			$user->name = 'admin';
+			$user->email = 'admin@admin.com';
+		}
+		$user->password = bcrypt('11223344');
+		$user->save();
+		echo "用户名:admin\n";
+		echo "密码:11223344\n";
+		echo "邮箱:admin@admin.com\n";
+		echo "重置Admin密码成功\n";
+	}
 }
